ab6dbc725843a362fb99df1c7521cf60d4867e1a
Zapdos26 <zapdoskid@gmail.com>
2020-02-10 16:52:20 -0500

    Connected RESPONDING/NOT RESPONDING messages to the main message. Moves main message down every time someone clicks a button.

--- utilities/Actions.js
+++ utilities/Actions.js
@@ -1,57 +1,58 @@
+const SlackIntegration = require('./SlackIntegration');
+const SlackIntegrationHelper = require('./SlackIntegrationHelper');
+
 require('dotenv').config();
 
 const MAX_ELAPSED_TIME = process.env.MAX_ELAPSED_TIME * 60 * 1000;
+const NAME_FORMAT = process.env.NAME_FORMAT;
 
-class Actions {
-    constructor(webclient) {
-        this.web = webclient;
-    }
-
-    postMessage(channel, blocks, color) {
-        const msg = {channel:channel, attachments:[{color:color, blocks:blocks}]};
-        return this.web.chat.postMessage(msg);
-    }
-
-    onButtonAction(payload, res) {
+class Actions extends SlackIntegration {
+    onButtonAction(payload) {
         try {
             const messageTime = new Date(payload.message.ts * 1000);
             const actionTime = new Date(payload.actions[0].action_ts * 1000);
             if ((actionTime - messageTime) < MAX_ELAPSED_TIME) {
-                this.web.users.info({user: payload.user.id}).then(resp => {
-                    const name = resp.user.real_name;
-                    if (payload.actions[0].action_id === 'statusYes') {
-                        const blocks = {
-                            type: 'section',
-                            block_id: 'responding',
-                            text: {
-                                type: 'mrkdwn',
-                                text: `*${name}* is *RESPONDING*.`
-                            }
-                        };
-                        this.postMessage(payload.container.channel_id,
-                            [blocks], '#7CD197');
-                    } else if (payload.actions[0].action_id === 'statusNo') {
-                        const blocks = {
-                            type: 'section',
-                            block_id: 'notresponding',
-                            text: {
-                                type: 'mrkdwn',
-                                text: `*${name}* is NOT RESPONDING.`
-                            }
-                        };
-                        this.postMessage(payload.container.channel_id,
-                            [blocks], '#d3d3d3');
+                payload.message.attachments.forEach((attachment, index) => {
+                    if (attachment.blocks[0].block_id === payload.user.id) {
+                        payload.message.attachments.splice(index, 1);
                     }
-                }
-                );
+
+                });
+                this.getSlackUser(payload.user.id).then(resp => {
+                    let name = resp.user.real_name;
+                    switch (NAME_FORMAT.toLowerCase()) {
+                    case 'short':
+                        const name_list = resp.user.real_name.split(' ');
+                        name = `${name_list[0].charAt(0)}. ` +
+                            `${name_list[name_list.length - 1]}`;
+                        break;
+                    case 'full':
+                        break;
+                    case 'mention':
+                        name = `<${payload.user.id}>`;
+                        break;
+                    }
+                    let blocks = {};
+                    let color = '';
+                    if (payload.actions[0].action_id === 'statusYes') {
+                        blocks = [SlackIntegrationHelper.buildSectionBlock(
+                            payload.user.id, `*${name}* is *RESPONDING*.`)];
+                        color = '#7CD197';
+                    } else if (payload.actions[0].action_id === 'statusNo') {
+                        blocks = [SlackIntegrationHelper.buildSectionBlock(
+                            payload.user.id, `*${name}* is NOT RESPONDING.`)];
+                        color = '#d3d3d3';
+                    }
+                    payload.message.attachments.push({blocks, color});
+                    this.deleteSlackMessage(payload.container.channel_id,
+                        payload.message.ts);
+                    this.postSlackMessageMA(payload.container.channel_id,
+                        payload.message.attachments);
+                });
             } else {
-                this.web.chat.postEphemeral({
-                    channel: payload.container.channel_id,
-                    text: 'Sorry, your response was logged too' +
-                        ' long after the dispatch went out.',
-                    user: payload.user.id
-                }
-                ).then();
+                this.postSlackEphemeral(payload.container.channel_id, 'Sorry, your' +
+                    ' response was logged too long after the dispatch went out.',
+                payload.user.id);
             }
         } catch (e) {
             res.status(500).send();
--- utilities/Notifications.js
+++ utilities/Notifications.js
@@ -1,48 +1,37 @@
 const NotificationHelper = require('./NotificationHelper');
+const SlackIntegration = require('./SlackIntegration');
 
 require('dotenv').config();
 
 const AIR_CHANNEL = process.env.AIR_CHANNEL;
 const DISPATCH_CHANNEL = process.env.DISPATCH_CHANNEL;
 
-class Notifications {
-    constructor(webclient) {
-        this.web = webclient;
-    }
-
-    postMessage(channel, blocks,color='#F35A00') {
-        const msg = {channel, attachments:[{color, blocks}]};
-        return this.web.chat.postMessage(msg);
-    }
-
-    updateMessage(channel, ts, blocks, color='#F35A00') {
-        const msg = {channel, ts, attachments:[{color, blocks}]};
-        return this.web.chat.update(msg);
-    }
+class Notifications extends SlackIntegration {
 
     normal(req, res) {
         if (NotificationHelper.verifyNightCrew()) {
-            this.postMessage(AIR_CHANNEL, NotificationHelper.nightCall(req));
-            this.postMessage(DISPATCH_CHANNEL, NotificationHelper.nightCall(req));
+            this.postSlackMessage(AIR_CHANNEL, NotificationHelper.nightCall(req));
+            this.postSlackMessage(DISPATCH_CHANNEL, NotificationHelper.nightCall(req));
         } else {
-            this.postMessage(AIR_CHANNEL, NotificationHelper.dayCall(req));
-            this.postMessage(DISPATCH_CHANNEL, [NotificationHelper.dispatchTime(),
-                NotificationHelper.dispatchMessage(req)]);
+            this.postSlackMessage(AIR_CHANNEL, NotificationHelper.dayCall(req));
+            this.postSlackMessage(DISPATCH_CHANNEL, [NotificationHelper.dispatchTime(),
+                NotificationHelper.normalMessage(req)]);
         }
         res.status(200).send();
     }
 
     longtone(req, res) {
-        this.postMessage(AIR_CHANNEL, NotificationHelper.longtoneMessage(req));
-        this.postMessage(DISPATCH_CHANNEL, NotificationHelper.longtoneMessage(req));
+        this.postSlackMessage(AIR_CHANNEL, NotificationHelper.longtoneMessage(req));
+        this.postSlackMessage(DISPATCH_CHANNEL, NotificationHelper.longtoneMessage(req));
         res.status(200).send();
     }
 
     email(data) {
-        this.web.conversations.history({channel:AIR_CHANNEL, limit:1}).then((result) => {
-            const blocks =result.messages[0].attachments[0].blocks;
-            blocks[1] = NotificationHelper.emailMessage(data);
-            this.updateMessage(AIR_CHANNEL, result.messages[0].ts, blocks);
+        this.getlatestSlackMessage(AIR_CHANNEL).then((result) => {
+            result.messages[0].attachments[0].blocks[1] =
+                NotificationHelper.emailMessage(data);
+            this.updateSlackMessageMA(AIR_CHANNEL, result.messages[0].ts,
+                result.messages[0].attachments);
         });
     }
 }
