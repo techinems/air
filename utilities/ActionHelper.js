0aa718be0e0b102364c5f0747f75c25487c2f041
Zapdos26 <zapdoskid@gmail.com>
2020-02-11 21:45:20 -0500

    Added ActionHelper

---
+++ utilities/ActionHelper.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const NAME_FORMAT = process.env.NAME_FORMAT;
+
+class ActionHelper {
+    static removeAttachmentByBlockId(attachments, block_id) {
+        attachments.forEach((attachment, index) => {
+            if (attachment.blocks[0].block_id === block_id) {
+                attachments.splice(index, 1);
+            }
+        });
+        return attachments;
+    }
+    static getFormattedName(user) {
+        let name = user.real_name;
+        switch (NAME_FORMAT.toLowerCase()) {
+        case 'short':
+            const name_list = user.real_name.split(' ');
+            name = `${name_list[0].charAt(0)}. ` +
+                `${name_list[name_list.length - 1]}`;
+            break;
+        case 'full':
+            break;
+        case 'mention':
+            name = `<${user.id}>`;
+            break;
+        }
+        return name;
+    }
+}
+
+module.exports = ActionHelper;
--- utilities/Actions.js
+++ utilities/Actions.js
@@ -1,53 +1,35 @@
 const SlackIntegration = require('./SlackIntegration');
 const SlackIntegrationHelper = require('./SlackIntegrationHelper');
+const ActionHelper = require('./ActionHelper');
 
 require('dotenv').config();
 
 const MAX_ELAPSED_TIME = process.env.MAX_ELAPSED_TIME * 60 * 1000;
-const NAME_FORMAT = process.env.NAME_FORMAT;
 
 class Actions extends SlackIntegration {
-    onButtonAction(payload) {
+    onButtonAction(payload, res) {
         try {
             const messageTime = new Date(payload.message.ts * 1000);
             const actionTime = new Date(payload.actions[0].action_ts * 1000);
             if ((actionTime - messageTime) < MAX_ELAPSED_TIME) {
-                payload.message.attachments.forEach((attachment, index) => {
-                    if (attachment.blocks[0].block_id === payload.user.id) {
-                        payload.message.attachments.splice(index, 1);
-                    }
-
-                });
+                const attachments = ActionHelper.removeAttachmentByBlockId(
+                    payload.message.attachments, payload.user.id);
                 this.getSlackUser(payload.user.id).then(resp => {
-                    let name = resp.user.real_name;
-                    switch (NAME_FORMAT.toLowerCase()) {
-                    case 'short':
-                        const name_list = resp.user.real_name.split(' ');
-                        name = `${name_list[0].charAt(0)}. ` +
-                            `${name_list[name_list.length - 1]}`;
-                        break;
-                    case 'full':
-                        break;
-                    case 'mention':
-                        name = `<${payload.user.id}>`;
-                        break;
-                    }
-                    let blocks = {};
-                    let color = '';
+                    const name = ActionHelper.getFormattedName(resp.user);
+                    let blocks = [];
+                    let color = '#7CD197';
                     if (payload.actions[0].action_id === 'statusYes') {
                         blocks = [SlackIntegrationHelper.buildSectionBlock(
                             payload.user.id, `*${name}* is *RESPONDING*.`)];
-                        color = '#7CD197';
                     } else if (payload.actions[0].action_id === 'statusNo') {
                         blocks = [SlackIntegrationHelper.buildSectionBlock(
                             payload.user.id, `*${name}* is NOT RESPONDING.`)];
-                        color = '#d3d3d3';
+                        color = '#D3D3D3';
                     }
-                    payload.message.attachments.push({blocks, color});
+                    attachments.push({blocks, color});
                     this.deleteSlackMessage(payload.container.channel_id,
                         payload.message.ts);
-                    this.postSlackMessageMA(payload.container.channel_id,
-                        payload.message.attachments);
+                    this.postSlackMessageMA(payload.container.channel_id, attachments);
                 });
             } else {
                 this.postSlackEphemeral(payload.container.channel_id, 'Sorry, your' +
